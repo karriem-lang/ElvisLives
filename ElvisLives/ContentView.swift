7fc51dc15b1ae5daaf7d04d3de545103dc5f1647
karriem-lang <karriemlang@proton.me>
2025-03-08 17:30:40 -0500

    Initial Commit

---
+++ ElvisLives/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ElvisLives
+//
+//  Created by Karriem Lang on 3/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ElvisLives/ElvisLivesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisLivesApp.swift
+//  ElvisLives
+//
+//  Created by Karriem Lang on 3/8/25.
+//
+
+import SwiftUI
+
+@main
+struct ElvisLivesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
